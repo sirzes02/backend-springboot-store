096e0b0643b8caea37bfa98600c532424ec6f3b9
S_Varela <sirzes02@gmail.com>
2022-08-06 15:51:06 -0500

    first commit

---
+++ src/main/java/com/backend/test/bankendstore/BackendStoreApplication.java
@@ -0,0 +1,11 @@
+package com.backend.test.bankendstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStoreApplication.class, args);
+    }
+}
---
+++ src/test/java/com/backend/test/bankendstore/BackendStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.test.bankendstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
